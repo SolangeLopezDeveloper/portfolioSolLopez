bd2d38a7db769985d68115da173331b2cc104e7d
SolangeLopez <solangelopezdev@gmail.com>
2023-06-24 16:28:19 -0300

    pruebas

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         resolve: "gatsby-source-filesystem",
         options: {
           name: "images",
-          path: `$/public/images/`,
+          path: `${__dirname}/public/images`,
         },
         __key: "images",
       },
